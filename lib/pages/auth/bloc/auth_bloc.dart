340e8ded3285066030eadf2e797f13816b364ce9
amallak01 <amallakshan01@gmail.com>
2022-10-11 14:41:32 +0530

    octtentwo

---
+++ lib/pages/auth/bloc/auth_bloc.dart
@@ -0,0 +1,17 @@
+import 'dart:async';
+
+import 'package:bloc/bloc.dart';
+import 'package:smart_ambulance/services/auth.dart';
+import 'package:equatable/equatable.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+part 'auth_event.dart';
+part 'auth_state.dart';
+
+class AuthBloc extends Bloc<AuthEvent, AuthState> {
+  AuthBloc() : super(AuthInitialState());
+
+  @override
+  Stream<AuthState> mapEventToState(AuthEvent event) async* {
+  }
+}
